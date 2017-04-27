7bfbfdad6b2b330152a2add4dc0c8003c0fcc5ce
Julian Pistorius <julian@julianpistorius.com>
2017-04-27 02:05:45 -0700

    Added new environment to .gitignore and now reading metric beats.

--- consume_stats.py
+++ consume_stats.py
@@ -1,7 +1,9 @@
 import json
+import pprint
+
 import kafka
 
-consumer = kafka.KafkaConsumer('glances', bootstrap_servers=['192.168.72.2:9092'], value_deserializer=json.loads)
+consumer = kafka.KafkaConsumer('metricsets', bootstrap_servers=['192.168.72.2:9092'], value_deserializer=json.loads)
 
 for s in consumer:
-    print(s)
+    pprint.pprint(s)
